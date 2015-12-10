5b48a6429818795baf0895f32c0fd16e1954f612
evoskuil <eric@voskuil.org>
2015-12-10 09:24:56 -0800

    Workaround missing API in test for MSVC builds.

--- src/bench.h
+++ src/bench.h
@@ -9,7 +9,17 @@
 
 #include <stdio.h>
 #include <math.h>
-#include "sys/time.h"
+
+#ifndef _MSC_VER
+#include <sys/time.h>
+#else
+#include <windows.h>
+
+// TODO: implement gettimeofday for windows builds.
+static double gettimeofday(struct timeval* tv, void* unused) {
+    return 0.0;
+}
+#endif
 
 static double gettimedouble(void) {
     struct timeval tv;
