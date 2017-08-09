6a1077eb4b0b8e517b8ba2cdabe94a989176219d
Andrew Poelstra <apoelstra@wpsoftware.net>
2017-08-09 23:48:07 +0000

    aggregate signatures: add module, implement single-user signing

--- src/tests.c
+++ src/tests.c
@@ -4679,6 +4679,10 @@ void run_ecdsa_openssl(void) {
 # include "modules/recovery/tests_impl.h"
 #endif
 
+#ifdef ENABLE_MODULE_AGGSIG
+# include "modules/aggsig/tests_impl.h"
+#endif
+
 int main(int argc, char **argv) {
     unsigned char seed16[16] = {0};
     unsigned char run32[32] = {0};
@@ -4797,6 +4801,10 @@ int main(int argc, char **argv) {
 #ifdef ENABLE_MODULE_RECOVERY
     /* ECDSA pubkey recovery tests */
     run_recovery_tests();
+#endif
+
+#ifdef ENABLE_MODULE_AGGSIG
+    run_aggsig_tests();
 #endif
 
     secp256k1_rand256(run32);
