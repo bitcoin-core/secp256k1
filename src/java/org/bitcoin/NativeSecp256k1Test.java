65de1ee0ff59ef1e4b8641ccec6e4386b217dbe6
liuyujun <liuyujun@fingera.cn>
2018-12-26 13:36:57 +0800

    add java test

--- src/java/org/bitcoin/NativeSecp256k1Test.java
+++ src/java/org/bitcoin/NativeSecp256k1Test.java
@@ -69,9 +69,12 @@ public class NativeSecp256k1Test {
     public static void testPubKeyCreatePos() throws AssertFailException{
         byte[] sec = BaseEncoding.base16().lowerCase().decode("67E56582298859DDAE725F972992A07C6C4FB9F62A8FFF58CE3CA926A1063530".toLowerCase());
 
-        byte[] resultArr = NativeSecp256k1.computePubkey( sec);
+        byte[] resultArr = NativeSecp256k1.computePubkey( sec, false);
         String pubkeyString = javax.xml.bind.DatatypeConverter.printHexBinary(resultArr);
         assertEquals( pubkeyString , "04C591A8FF19AC9C4E4E5793673B83123437E975285E7B442F4EE2654DFFCA5E2D2103ED494718C697AC9AEBCFD19612E224DB46661011863ED2FC54E71861E2A6" , "testPubKeyCreatePos");
+        resultArr = NativeSecp256k1.computePubkey( sec, true);
+        pubkeyString = javax.xml.bind.DatatypeConverter.printHexBinary(resultArr);
+        assertEquals( pubkeyString , "02C591A8FF19AC9C4E4E5793673B83123437E975285E7B442F4EE2654DFFCA5E2D" , "testPubKeyCreatePos");
     }
 
     /**
@@ -80,7 +83,7 @@ public class NativeSecp256k1Test {
     public static void testPubKeyCreateNeg() throws AssertFailException{
        byte[] sec = BaseEncoding.base16().lowerCase().decode("FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF".toLowerCase());
 
-       byte[] resultArr = NativeSecp256k1.computePubkey( sec);
+       byte[] resultArr = NativeSecp256k1.computePubkey( sec, false);
        String pubkeyString = javax.xml.bind.DatatypeConverter.printHexBinary(resultArr);
        assertEquals( pubkeyString, "" , "testPubKeyCreateNeg");
     }
