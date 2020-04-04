ba796036c045683874464151e06d3709f3e6c248
Jonas Nick <jonasd.nick@gmail.com>
2020-04-04 15:52:01 +0000

    Move R from adaptor_proof to adaptor_sig and fix args of _adapt

--- include/secp256k1_ecdsa_adaptor.h
+++ include/secp256k1_ecdsa_adaptor.h
@@ -5,24 +5,23 @@
 extern "C" {
 #endif
 
-
-/* note: adaptor_proof129 is 129 because 1*R + 1*R' + 1*e + 1*x = 2*32 + 1 + 32 + 32 */
+/* note: adaptor_proof97 is 97 because 1*R' + 1*e + 1*s_dleq = 33 + 32 + 32 */
 
 /** Adaptor sign
  *  TODO: add nonce function
  *
  *  Returns: 1 on success, 0 on failure
  *  Args:             ctx: a secp256k1 context object
- *  Out:    adaptor_sig32: pointer to 32-byte to store the returned signature
- *       adaptor_proof129: pointer to 129-byte to store the adaptor proof
+ *  Out:    adaptor_sig65: pointer to 65-byte to store the returned signature
+ *       adaptor_proof97: pointer to 97-byte to store the adaptor proof
  *  In:           adaptor: adaptor point
  *                  msg32: the 32-byte message to sign
  *
  */
 SECP256K1_API int secp256k1_ecdsa_adaptor_sign(
     const secp256k1_context* ctx,
-    unsigned char *adaptor_sig32,
-    unsigned char *adaptor_proof129,
+    unsigned char *adaptor_sig65,
+    unsigned char *adaptor_proof97,
     const secp256k1_pubkey *adaptor,
     const unsigned char *msg32
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4);
@@ -31,20 +30,20 @@ SECP256K1_API int secp256k1_ecdsa_adaptor_sign(
  *
  *  Returns: 1 on success, 0 on failure
  *  Args:             ctx: a secp256k1 context object
- *  In:     adaptor_sig32: pointer to 32-byte signature to verify
+ *  In:     adaptor_sig65: pointer to 65-byte signature to verify
  *                 pubkey: pubkey
  *                  msg32: 32-byte message
  *                adaptor: adaptor point
- *       adaptor_proof129: pointer to 129-byte adaptor proof
+ *       adaptor_proof97: pointer to 97-byte adaptor proof
  *
  */
 SECP256K1_API int secp256k1_ecdsa_adaptor_sig_verify(
     const secp256k1_context* ctx,
-    const unsigned char *adaptor_sig32,
+    const unsigned char *adaptor_sig65,
     const secp256k1_pubkey *pubkey,
     const unsigned char *msg32,
     const secp256k1_pubkey *adaptor,
-    const unsigned char *adaptor_proof129,
+    const unsigned char *adaptor_proof97,
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4) SECP256K1_ARG_NONNULL(5) SECP256K1_ARG_NONNULL(6);
 
 /** Adapt aka complete
@@ -53,14 +52,14 @@ SECP256K1_API int secp256k1_ecdsa_adaptor_sig_verify(
  *  Args:             ctx: a secp256k1 context object
  *  Out:              sig: ecdsa signature
  *  In:  adaptor_secret32: pointer to 32-byte byte adaptor secret of the adaptor point
- *       adaptor_proof129: pointer to 129-byte byte adaptor proof
+ *          adaptor_sig65: pointer to 65-byte byte adaptor sig
  *
  */
- SECP256K1_API int secp256k1_ecdsa_adaptor_adapt(
+SECP256K1_API int secp256k1_ecdsa_adaptor_adapt(
     const secp256k1_context* ctx,
     secp256k1_ecdsa_signature *sig,
     const unsigned char *adaptor_secret32,
-    const unsigned char *adaptor_proof129,
+    const unsigned char *adaptor_sig65
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4);
 
 /** Adaptor extract
@@ -77,7 +76,7 @@ SECP256K1_API int secp256k1_ecdsa_adaptor_extract_secret(
     const secp256k1_context* ctx,
     unsigned char *adaptor_secret32,
     const secp256k1_ecdsa_signature *sig,
-    const unsigned char *adaptor_sig32,
+    const unsigned char *adaptor_sig65,
     const secp256k1_pubkey *adaptor
 ) SECP256K1_ARG_NONNULL(1) SECP256K1_ARG_NONNULL(2) SECP256K1_ARG_NONNULL(3) SECP256K1_ARG_NONNULL(4) SECP256K1_ARG_NONNULL(5);
 
@@ -86,4 +85,3 @@ SECP256K1_API int secp256k1_ecdsa_adaptor_extract_secret(
 #endif
 
 #endif /* SECP256K1_ADAPTOR_H */
-
