bb36fe9be0998c81ebc9f18e122bb7617d919877
Tim Ruffing <crypto@timruffing.de>
2021-11-13 15:52:44 +0100

    ci: Test `make precomp`

--- ci/cirrus.sh
+++ ci/cirrus.sh
@@ -52,7 +52,19 @@ then
         $EXEC ./bench
     } >> bench.log 2>&1
 fi
+
 if [ "$CTIMETEST" = "yes" ]
 then
     ./libtool --mode=execute valgrind --error-exitcode=42 ./valgrind_ctime_test > valgrind_ctime_test.log 2>&1
 fi
+
+# Rebuild precomputed files (if not cross-compiling).
+if [ -z "$HOST" ]
+then
+    make clean-precomp
+    make precomp
+fi
+
+# Check that no repo files have been modified by the build.
+# (This fails for example if the precomp files need to be updated in the repo.)
+git diff --exit-code
