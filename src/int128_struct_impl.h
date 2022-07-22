2fe03cda237cb362957cd0a901906b4d097a17e1
Pieter Wuille <pieter@wuille.net>
2022-07-22 11:01:20 -0400

    Switch to int128_struct implementation using (u)mul128 abstractions

--- src/int128_struct_impl.h
+++ src/int128_struct_impl.h
@@ -4,40 +4,61 @@
 #include "int128.h"
 
 #if defined(_MSC_VER) && (defined(_M_X64) || defined(_M_ARM64)) /* MSVC */
-  #include <intrin.h>
-  #define secp256k1_umulh __umulh
-  #define secp256k1_mulh __mulh
-#else
-static SECP256K1_INLINE uint64_t secp256k1_umulh(uint64_t a, uint64_t b) {
-   uint64_t t1 = (uint64_t)(uint32_t)a * (uint32_t)b;
-   uint64_t t2 = (a >> 32) * (uint32_t)b;
-   uint64_t t3 = (uint32_t)a*(b >> 32) + (t1 >> 32) + (uint32_t)t2;
-   return (a >> 32)*(b >> 32) + (t2 >> 32) + (t3 >> 32);
+#    if defined(_M_X64)
+/* On x84_64 MSVC, use native _(u)mul128 for 64x64->128 multiplications. */
+#        include <intrin.h>
+#        define secp256k1_umul128 _umul128
+#        define secp256k1_mul128 _mul128
+#    else
+#        include <intrin.h>
+/* On ARM64 MSVC, use __(u)mulh for the upper half of 64x64 multiplications. */
+static SECP256K1_INLINE uint64_t secp256k1_umul128(uint64_t a, uint64_t b, uint64_t* hi) {
+    *hi = __umulh(a, b);
+    return a * b;
 }
 
-static SECP256K1_INLINE int64_t secp256k1_mulh(int64_t a, int64_t b) {
-   uint64_t t1 = (uint64_t)(uint32_t)a * (uint32_t)b;
-   int64_t t2 = (a >> 32) * (uint32_t)b;
-   int64_t t3 = (uint32_t)a * (b >> 32);
-   uint64_t t4 = (t1 >> 32) + (uint32_t)t2 + (uint32_t)t3;
-   return (a >> 32) * (b >> 32) + (t2 >> 32) + (t3 >> 32) + (int64_t)(t4 >> 32);
+static SECP256K1_INLINE int64_t secp256k1_mul128(int64_t a, int64_t b, int64_t* hi) {
+    *hi = __mulh(a, b);
+    return a * b;
+}
+#    endif
+#else
+/* On other systems, emulate 64x64->128 multiplications using 32x32->64 multiplications. */
+static SECP256K1_INLINE uint64_t secp256k1_umul128(uint64_t a, uint64_t b, uint64_t* hi) {
+    uint64_t ll = (uint64_t)(uint32_t)a * (uint32_t)b;
+    uint64_t lh = (uint32_t)a * (b >> 32);
+    uint64_t hl = (a >> 32) * (uint32_t)b;
+    uint64_t hh = (a >> 32) * (b >> 32);
+    uint64_t mid34 = (ll >> 32) + (uint32_t)lh + (uint32_t)hl;
+    *hi = hh + (lh >> 32) + (hl >> 32) + (mid34 >> 32);
+    return (mid34 << 32) + (uint32_t)ll;
+}
+
+static SECP256K1_INLINE int64_t secp256k1_mul128(int64_t a, int64_t b, int64_t* hi) {
+    uint64_t ll = (uint64_t)(uint32_t)a * (uint32_t)b;
+    int64_t lh = (uint32_t)a * (b >> 32);
+    int64_t hl = (a >> 32) * (uint32_t)b;
+    int64_t hh = (a >> 32) * (b >> 32);
+    uint64_t mid34 = (ll >> 32) + (uint32_t)lh + (uint32_t)hl;
+    *hi = hh + (lh >> 32) + (hl >> 32) + (mid34 >> 32);
+    return (mid34 << 32) + (uint32_t)ll;
 }
 #endif
 
 static SECP256K1_INLINE void secp256k1_u128_mul(secp256k1_uint128 *r, uint64_t a, uint64_t b) {
-   r->hi = secp256k1_umulh(a, b);
-   r->lo = a * b;
+   r->lo = secp256k1_umul128(a, b, &r->hi);
 }
 
 static SECP256K1_INLINE void secp256k1_u128_accum_mul(secp256k1_uint128 *r, uint64_t a, uint64_t b) {
-   uint64_t lo = a * b;
-   r->hi += secp256k1_umulh(a, b) + (~lo < r->lo);
+   uint64_t lo, hi;
+   lo = secp256k1_umul128(a, b, &hi);
    r->lo += lo;
+   r->hi += hi + (r->lo < lo);
 }
 
 static SECP256K1_INLINE void secp256k1_u128_accum_u64(secp256k1_uint128 *r, uint64_t a) {
-   r->hi += (r->lo > ~a);
    r->lo += a;
+   r->hi += r->lo < a;
 }
 
 /* Unsigned (logical) right shift.
@@ -46,7 +67,7 @@ static SECP256K1_INLINE void secp256k1_u128_accum_u64(secp256k1_uint128 *r, uint
 static SECP256K1_INLINE void secp256k1_u128_rshift(secp256k1_uint128 *r, unsigned int n) {
    VERIFY_CHECK(n < 128);
    if (n >= 64) {
-     r->lo = (r->hi) >> (n-64);
+     r->lo = r->hi >> (n-64);
      r->hi = 0;
    } else if (n > 0) {
      r->lo = ((1U * r->hi) << (64-n)) | r->lo >> n;
@@ -74,41 +95,44 @@ static SECP256K1_INLINE int secp256k1_u128_check_bits(const secp256k1_uint128 *r
 }
 
 static SECP256K1_INLINE void secp256k1_i128_mul(secp256k1_int128 *r, int64_t a, int64_t b) {
-   r->hi = (uint64_t)secp256k1_mulh(a, b);
-   r->lo = (uint64_t)a * (uint64_t)b;
+   int64_t hi;
+   r->lo = (uint64_t)secp256k1_mul128(a, b, &hi);
+   r->hi = (uint64_t)hi;
 }
 
 static SECP256K1_INLINE void secp256k1_i128_accum_mul(secp256k1_int128 *r, int64_t a, int64_t b) {
-   uint64_t lo = (uint64_t)a * (uint64_t)b;
-   uint64_t hi = (uint64_t)secp256k1_mulh(a, b) + (~lo < r->lo);
+   int64_t hi;
+   uint64_t lo = (uint64_t)secp256k1_mul128(a, b, &hi);
+   r->lo += lo;
+   hi += r->lo < lo;
    /* Verify no overflow.
     * If r represents a positive value (the sign bit is not set) and the value we are adding is a positive value (the sign bit is not set),
     * then we require that the resulting value also be positive (the sign bit is not set).
     * Note that (X <= Y) means (X implies Y) when X and Y are boolean values (i.e. 0 or 1).
     */
-   VERIFY_CHECK((r->hi <= 0x7fffffffffffffffu && hi <= 0x7fffffffffffffffu) <= (r->hi + hi <= 0x7fffffffffffffffu));
+   VERIFY_CHECK((r->hi <= 0x7fffffffffffffffu && (uint64_t)hi <= 0x7fffffffffffffffu) <= (r->hi + (uint64_t)hi <= 0x7fffffffffffffffu));
    /* Verify no underflow.
     * If r represents a negative value (the sign bit is set) and the value we are adding is a negative value (the sign bit is set),
     * then we require that the resulting value also be negative (the sign bit is set).
     */
-   VERIFY_CHECK((r->hi > 0x7fffffffffffffffu && hi > 0x7fffffffffffffffu) <= (r->hi + hi > 0x7fffffffffffffffu));
+   VERIFY_CHECK((r->hi > 0x7fffffffffffffffu && (uint64_t)hi > 0x7fffffffffffffffu) <= (r->hi + (uint64_t)hi > 0x7fffffffffffffffu));
    r->hi += hi;
-   r->lo += lo;
 }
 
 static SECP256K1_INLINE void secp256k1_i128_dissip_mul(secp256k1_int128 *r, int64_t a, int64_t b) {
-   uint64_t lo = (uint64_t)a * (uint64_t)b;
-   uint64_t hi = (uint64_t)secp256k1_mulh(a, b) + (r->lo < lo);
+   int64_t hi;
+   uint64_t lo = (uint64_t)secp256k1_mul128(a, b, &hi);
+   hi += r->lo < lo;
    /* Verify no overflow.
     * If r represents a positive value (the sign bit is not set) and the value we are subtracting is a negative value (the sign bit is set),
     * then we require that the resulting value also be positive (the sign bit is not set).
     */
-   VERIFY_CHECK((r->hi <= 0x7fffffffffffffffu && hi > 0x7fffffffffffffffu) <= (r->hi - hi <= 0x7fffffffffffffffu));
+   VERIFY_CHECK((r->hi <= 0x7fffffffffffffffu && (uint64_t)hi > 0x7fffffffffffffffu) <= (r->hi - (uint64_t)hi <= 0x7fffffffffffffffu));
    /* Verify no underflow.
     * If r represents a negative value (the sign bit is set) and the value we are subtracting is a positive value (the sign sign bit is not set),
     * then we require that the resulting value also be negative (the sign bit is set).
     */
-   VERIFY_CHECK((r->hi > 0x7fffffffffffffffu && hi <= 0x7fffffffffffffffu) <= (r->hi - hi > 0x7fffffffffffffffu));
+   VERIFY_CHECK((r->hi > 0x7fffffffffffffffu && (uint64_t)hi <= 0x7fffffffffffffffu) <= (r->hi - (uint64_t)hi > 0x7fffffffffffffffu));
    r->hi -= hi;
    r->lo -= lo;
 }
