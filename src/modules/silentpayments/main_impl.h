8fe6ab62a2ada4aa7eb21cb3314812b7bc3c898e
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2023-09-27 17:22:14 +0200

    build: add skeleton for new silentpayments (BIP352) module

---
+++ src/modules/silentpayments/main_impl.h
@@ -0,0 +1,16 @@
+/***********************************************************************
+ * Distributed under the MIT software license, see the accompanying    *
+ * file COPYING or https://www.opensource.org/licenses/mit-license.php.*
+ ***********************************************************************/
+
+#ifndef SECP256K1_MODULE_SILENTPAYMENTS_MAIN_H
+#define SECP256K1_MODULE_SILENTPAYMENTS_MAIN_H
+
+#include "../../../include/secp256k1.h"
+#include "../../../include/secp256k1_silentpayments.h"
+
+/* TODO: implement functions for sender side. */
+
+/* TODO: implement functions for receiver side. */
+
+#endif
