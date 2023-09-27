a9a5fe8e28f2f4dd53fdfb4ce797b23b4452f0a6
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2023-09-27 17:22:14 +0200

    build: add skeleton for new silentpayments (BIP352) module

---
+++ include/secp256k1_silentpayments.h
@@ -0,0 +1,20 @@
+#ifndef SECP256K1_SILENTPAYMENTS_H
+#define SECP256K1_SILENTPAYMENTS_H
+
+#include "secp256k1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* TODO: add module description */
+
+/* TODO: add function API for sender side. */
+
+/* TODO: add function API for receiver side. */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SECP256K1_SILENTPAYMENTS_H */
