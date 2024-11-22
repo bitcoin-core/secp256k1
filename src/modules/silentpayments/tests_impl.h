90630fb28447e020eee1019847034bec0119273d
stratospher <44024636+stratospher@users.noreply.github.com>
2024-11-22 13:03:52 +0530

    add dleq implementation
    - modify secp256k1-zkp's dleq implementation to be consistent with
      BIP 374.
    - use BIP374 notations.
    - add DLEQ tests

--- src/modules/silentpayments/tests_impl.h
+++ src/modules/silentpayments/tests_impl.h
@@ -7,6 +7,7 @@
 #define SECP256K1_MODULE_SILENTPAYMENTS_TESTS_H
 
 #include "../../../include/secp256k1_silentpayments.h"
+#include "../../../src/modules/silentpayments/dleq_impl.h"
 #include "../../../src/modules/silentpayments/vectors.h"
 
 /** Constants
@@ -760,6 +761,113 @@ void run_silentpayments_test_vectors(void) {
     }
 }
 
+static void dleq_nonce_bitflip(unsigned char **args, size_t n_flip, size_t n_bytes) {
+    secp256k1_scalar k1, k2;
+    CHECK(secp256k1_dleq_nonce(&k1, args[0], args[1], args[2], args[3]) == 1);
+    testrand_flip(args[n_flip], n_bytes);
+    CHECK(secp256k1_dleq_nonce(&k2, args[0], args[1], args[2], args[3]) == 1);
+    CHECK(secp256k1_scalar_eq(&k1, &k2) == 0);
+}
+
+static void dleq_tests(void) {
+    secp256k1_scalar s, e, a, k;
+    secp256k1_ge A, B, C;
+    unsigned char *args[4];
+    unsigned char a32[32];
+    unsigned char A_33[33];
+    unsigned char C_33[33];
+    unsigned char aux_rand[32];
+    unsigned char msg[32];
+    unsigned char proof_64[64] = {0};
+    int i;
+    size_t pubkey_size = 33;
+    int overflow;
+    secp256k1_sha256 sha;
+    secp256k1_sha256 sha_optimized;
+    unsigned char aux_tag[] = {'B', 'I', 'P', '0', '3', '7', '4', '/', 'a', 'u', 'x'};
+    unsigned char tag[] = {'B', 'I', 'P', '0', '3', '7', '4', '/', 'n', 'o', 'n', 'c', 'e'};
+    unsigned char challenge_tag[] = {'B', 'I', 'P', '0', '3', '7', '4', '/', 'c', 'h', 'a', 'l', 'l', 'e', 'n', 'g', 'e'};
+
+    /* Check that hash initialized by secp256k1_nonce_function_bip374_sha256_tagged_aux has the expected state. */
+    secp256k1_sha256_initialize_tagged(&sha, aux_tag, sizeof(aux_tag));
+    secp256k1_nonce_function_bip374_sha256_tagged_aux(&sha_optimized);
+    test_sha256_eq(&sha, &sha_optimized);
+
+    /* Check that hash initialized by secp256k1_nonce_function_bip374_sha256_tagged has the expected state. */
+    secp256k1_sha256_initialize_tagged(&sha, tag, sizeof(tag));
+    secp256k1_nonce_function_bip374_sha256_tagged(&sha_optimized);
+    test_sha256_eq(&sha, &sha_optimized);
+
+    /* Check that hash initialized by secp256k1_dleq_sha256_tagged has the expected state. */
+    secp256k1_sha256_initialize_tagged(&sha, challenge_tag, sizeof(challenge_tag));
+    secp256k1_dleq_sha256_tagged(&sha_optimized);
+    test_sha256_eq(&sha, &sha_optimized);
+
+    for (i = 0; i < COUNT; i++) {
+        testutil_random_ge_test(&B);
+        testutil_random_scalar_order(&a);
+        testrand256(aux_rand);
+        testrand_bytes_test(msg, sizeof(msg));
+        secp256k1_dleq_pair(&CTX->ecmult_gen_ctx, &A, &C, &a, &B);
+        CHECK(secp256k1_dleq_prove(CTX, &s, &e, &a, &B, &A, &C, aux_rand, (i & 1) ? msg : NULL) == 1);
+        CHECK(secp256k1_dleq_verify(&s, &e, &A, &B, &C, (i & 1) ? msg : NULL) == 1);
+        secp256k1_scalar_set_b32(&s, proof_64, &overflow);
+        VERIFY_CHECK(overflow == 0);
+        secp256k1_scalar_set_b32(&e, proof_64 + 32, &overflow);
+        VERIFY_CHECK(overflow == 0);
+    }
+
+    {
+        secp256k1_scalar tmp;
+        secp256k1_scalar_set_int(&tmp, 1);
+        CHECK(secp256k1_dleq_verify(&tmp, &e, &A, &B, &C, msg) == 0);
+        CHECK(secp256k1_dleq_verify(&s, &tmp, &A, &B, &C, msg) == 0);
+    }
+    {
+        secp256k1_ge p_tmp;
+        testutil_random_ge_test(&p_tmp);
+        CHECK(secp256k1_dleq_verify(&s, &e, &p_tmp, &B, &C, msg) == 0);
+        CHECK(secp256k1_dleq_verify(&s, &e, &A, &p_tmp, &C, msg) == 0);
+        CHECK(secp256k1_dleq_verify(&s, &e, &A, &B, &p_tmp, msg) == 0);
+    }
+    {
+        secp256k1_ge p_inf;
+        secp256k1_ge_set_infinity(&p_inf);
+        CHECK(secp256k1_dleq_prove(CTX, &s, &e, &a, &p_inf, &A, &C, aux_rand, msg) == 0);
+        CHECK(secp256k1_dleq_prove(CTX, &s, &e, &a, &B, &p_inf, &C, aux_rand, msg) == 0);
+        CHECK(secp256k1_dleq_prove(CTX, &s, &e, &a, &B, &A, &p_inf, aux_rand, msg) == 0);
+    }
+
+    /* Nonce tests */
+    secp256k1_scalar_get_b32(a32, &a);
+    CHECK(secp256k1_eckey_pubkey_serialize(&A, A_33, &pubkey_size, 1));
+    CHECK(secp256k1_eckey_pubkey_serialize(&C, C_33, &pubkey_size, 1));
+    CHECK(secp256k1_dleq_nonce(&k, a32, A_33, C_33, aux_rand) == 1);
+
+    testrand_bytes_test(a32, sizeof(a32));
+    testrand_bytes_test(A_33, sizeof(A_33));
+    testrand_bytes_test(C_33, sizeof(C_33));
+    testrand_bytes_test(aux_rand, sizeof(aux_rand));
+
+    /* Check that a bitflip in an argument results in different nonces. */
+    args[0] = a32;
+    args[1] = A_33;
+    args[2] = C_33;
+    args[3] = aux_rand;
+    for (i = 0; i < COUNT; i++) {
+        dleq_nonce_bitflip(args, 0, sizeof(a32));
+        dleq_nonce_bitflip(args, 1, sizeof(A_33));
+        /* Flip C */
+        dleq_nonce_bitflip(args, 2, sizeof(C_33));
+        /* Flip C again */
+        dleq_nonce_bitflip(args, 2, sizeof(C_33));
+        dleq_nonce_bitflip(args, 3, sizeof(aux_rand));
+    }
+
+    /* NULL aux_rand argument is allowed.*/
+    CHECK(secp256k1_dleq_nonce(&k, a32, A_33, C_33, NULL) == 1);
+}
+
 void run_silentpayments_tests(void) {
     test_recipient_sort();
     test_send_api();
@@ -767,6 +875,7 @@ void run_silentpayments_tests(void) {
     test_recipient_api();
     run_silentpayments_test_vectors();
     silentpayments_sha256_tag_test();
+    dleq_tests();
 }
 
 #endif
