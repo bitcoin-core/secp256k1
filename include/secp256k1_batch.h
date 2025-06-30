0b6b0c87add4e538b9ec2fda488381e7a27d81b4
siv2r <siv2ram@gmail.com>
2025-06-30 18:00:06 +0530

    batch: Initialize an experimental batch module
    This commit adds the foundational configuration, build scripts,
    and an initial structure for experimental batch module.

---
+++ include/secp256k1_batch.h
@@ -0,0 +1,25 @@
+#ifndef SECP256K1_BATCH_H
+#define SECP256K1_BATCH_H
+
+#include "secp256k1.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/** This module implements a Batch Verification object that supports:
+ *
+ *  1. Schnorr signatures compliant with Bitcoin Improvement Proposal 340
+ *     "Schnorr Signatures for secp256k1"
+ *     (https://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki).
+ *
+ *  2. Taproot commitments compliant with Bitcoin Improvement Proposal 341
+ *     "Taproot: SegWit version 1 spending rules"
+ *     (https://github.com/bitcoin/bips/blob/master/bip-0341.mediawiki).
+ */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SECP256K1_BATCH_H */
