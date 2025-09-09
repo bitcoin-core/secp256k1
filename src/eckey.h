c0c9acb77e934c478ad1251dddb2b7f5c9badeaa
Fabian Jahr <fjahr@protonmail.com>
2025-09-09 23:51:40 +0200

    refactor: Move parsing helpers from musig to eckey
    The naming of these functions suggested they should be moved to group.h
    but this didn't seem practical since these functions depended on eckey.h
    internally which is higher level than group.h.

--- src/eckey.h
+++ src/eckey.h
@@ -17,6 +17,9 @@
 static int secp256k1_eckey_pubkey_parse(secp256k1_ge *elem, const unsigned char *pub, size_t size);
 static int secp256k1_eckey_pubkey_serialize(secp256k1_ge *elem, unsigned char *pub, size_t *size, int compressed);
 
+static void secp256k1_eckey_serialize_ext(unsigned char *out33, secp256k1_ge* ge);
+static int secp256k1_eckey_parse_ext(secp256k1_ge* ge, const unsigned char *in33);
+
 static int secp256k1_eckey_privkey_tweak_add(secp256k1_scalar *key, const secp256k1_scalar *tweak);
 static int secp256k1_eckey_pubkey_tweak_add(secp256k1_ge *key, const secp256k1_scalar *tweak);
 static int secp256k1_eckey_privkey_tweak_mul(secp256k1_scalar *key, const secp256k1_scalar *tweak);
