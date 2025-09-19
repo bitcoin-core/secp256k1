c07e710003c84f206a68fe0cfaa6c5a06ce5fc37
siv2r <siv2ram@gmail.com>
2025-09-19 19:34:28 +0530

    batch: remove experimental status

--- doc/speedup-batch/bench.sh
+++ doc/speedup-batch/bench.sh
@@ -7,7 +7,7 @@ cd ../../
 echo "HEAD: $(git rev-parse --short HEAD)" > "$cur_dir/$output_file.log"
 make clean
 ./autogen.sh
-./configure --enable-experimental --enable-module-batch --enable-module-schnorrsig >> "$cur_dir/$output_file.log"
+./configure --enable-module-batch --enable-module-schnorrsig >> "$cur_dir/$output_file.log"
 make
 ./bench schnorrsig > "$cur_dir/$output_file"
 ./bench extrakeys >> "$cur_dir/$output_file"
